ef05faf89fb761b673a9c6ce7987b70366443846
Elena Shestakova <ShestakovaElenaA@yandex.ru>
2023-10-09 07:44:01 +0300

    code without exception

---
+++ src/test/java/ru/netology/Exception/ShopRepositoryTest.java
@@ -0,0 +1,34 @@
+package ru.netology.Exception;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+    @Test
+    public void removeNonExistentObjectTest(){
+       Product[] products = new Product[2];
+       Product product1 = new Product(1,"йогурт",55);
+       Product product2 = new Product(2,"булка",45);
+       ShopRepository shopRepository = new ShopRepository();
+       shopRepository.add(product1);
+       shopRepository.add(product2);
+       shopRepository.remove(3);
+
+          }
+    @Test
+    public void removeExistentObjectTest(){
+        Product[] products = new Product[2];
+        Product product1 = new Product(1,"йогурт",55);
+        Product product2 = new Product(2,"булка",45);
+        ShopRepository shopRepository = new ShopRepository();
+        shopRepository.add(product1);
+        shopRepository.add(product2);
+        shopRepository.remove(2);
+
+        Product[] expected = {product1};
+        Product[] atual = shopRepository.findAll();
+
+        Assertions.assertArrayEquals(expected,atual);
+
+    }
+}
